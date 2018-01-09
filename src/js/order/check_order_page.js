caba696678fe1735d1572cfc4b9b00c0fa01a1a3
KarenQiao <498101094@qq.com>
2018-01-09 11:05:14 +0800

    提交页面

--- src/js/order/check_order_page.js
+++ src/js/order/check_order_page.js
@@ -43,7 +43,11 @@ $(function(){
                 url:urL + coupon + info.data.couponsId,
                 success:function(info1){
                     if(info1.status !== 200){
-                        alert(info1.mag);
+
+                        $('.couponMsg').html('暂无优惠')
+
+                        console.log(info1.mag)
+                        //alert(info1.mag);
                         return;
                     }
                     console.log(info1);
@@ -74,8 +78,36 @@ $(function(){
             //去支付
             $('.payMode').click(function(){
                 if( info.data.payMode == '1'){
+
+                    function onBridgeReady(){
+                        WeixinJSBridge.invoke(
+                            'getBrandWCPayRequest', {
+                                "appId":"wxa65cb239c6a0b470",     //公众号名称，由商户传入
+                                "timeStamp":"1395712654",         //时间戳，自1970年以来的秒数
+                                "nonceStr":"e61463f8efa94090b1f366cccfbbb444", //随机串
+                                "package":"prepay_id=u802345jgfjsdfgsdg888",
+                                "signType":"MD5",         //微信签名方式：
+                                "paySign":"70EA570631E4BB79628FBCA90534C63FF7FADD89" //微信签名
+                            },
+                            function(res){
+                                if(res.err_msg == "get_brand_wcpay_request:ok" ) {}     // 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回    ok，但并不保证它绝对可靠。
+                            }
+                        );
+                    }
+                    if (typeof WeixinJSBridge == "undefined"){
+                        if( document.addEventListener ){
+                            document.addEventListener('WeixinJSBridgeReady', onBridgeReady, false);
+                        }else if (document.attachEvent){
+                            document.attachEvent('WeixinJSBridgeReady', onBridgeReady);
+                            document.attachEvent('onWeixinJSBridgeReady', onBridgeReady);
+                        }
+                    }else{
+                        onBridgeReady();
+                    }
+
+
                     //微信
-                    location.href = '../../html/order/shopping_order_pay.html';
+                   // location.href = '../../html/order/shopping_order_pay.html';
                 }else if(info.data.payMode == '2'){
                     //支付宝
                     location.href = '../../html/order/shopping_order_pay.html';
